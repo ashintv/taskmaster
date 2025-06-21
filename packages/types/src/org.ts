c24dab31018f9a4084fd186de63ab70332674397
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-06-21 15:58:15 +0530

    package-zod-org

---
+++ packages/types/src/org.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { passwordSchema, usernameSchema } from "./util";
+
+
+export const Signin = z.object({
+    username: usernameSchema,
+    password:passwordSchema
+});
+
+export const OrgCompleteForm = z.object({
+        name:z.string().min(4, "must contain atleast 4 charecters"),
+        email:z.string().email("Incorrect e mail format"),
+        address:z.string(),
+        image:z.string().url()
+
+})
+
+export const createOfficeForm = z.object({
+    name:z.string(),
+    username:usernameSchema,
+    desc:z.string()
+
+})
---
+++ packages/types/src/util.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const passwordSchema = z
+  .string()
+  .min(8, "Password must be at least 8 characters")
+  .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Must contain at least one digit")
+  .regex(/[@$!%*?&]/, "Must contain at least one special character");
+
+
+export const usernameSchema = z
+  .string()
+  .min(3, "Username must be at least 3 characters")
+  .max(20, "Username must be at most 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores");
